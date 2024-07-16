77b7d86bd408f2aa2f09d13c2146f7e8972aa3f6
ben-dh3 <benjaminduffield1997@gmail.com>
2024-07-16 16:24:05 +0100

    gestures

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { useState } from 'react';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
@@ -52,7 +53,7 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
        <ImageViewer
           placeholderImageSource={PlaceholderImage}
           selectedImage={selectedImage}
@@ -76,7 +77,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
